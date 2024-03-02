daaf998d6b5f657b4f75957cc3bef8a1d518d4cb
adidev-sxb18 <adidev.sudoxbyte@gmail.com>
2024-03-03 00:55:52 +0530

    TON_Vim-1

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,12 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  "mellow-theme/mellow.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "mellow"
+  vim.g.mellow_transparent = true
 end
 
 return M
